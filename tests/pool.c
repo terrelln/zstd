71e6c593090359cd2372d0a2aca9f52f62b9ade7
Nick Terrell <terrelln@fb.com>
2016-12-31 20:08:52 -0500

    Shorten test

--- tests/pool.c
+++ tests/pool.c
@@ -14,7 +14,7 @@
 
 struct data {
   pthread_mutex_t mutex;
-  unsigned data[1024];
+  unsigned data[128];
   size_t i;
 };
 
@@ -34,12 +34,12 @@ int testOrder(size_t numThreads, size_t queueLog) {
   pthread_mutex_init(&data.mutex, NULL);
   {
     size_t i;
-    for (i = 0; i < 1024; ++i) {
+    for (i = 0; i < 128; ++i) {
       POOL_add(ctx, &fn, &data);
     }
   }
   POOL_free(ctx);
-  ASSERT_EQ(1024, data.i);
+  ASSERT_EQ(128, data.i);
   {
     size_t i;
     for (i = 0; i < data.i; ++i) {
@@ -52,19 +52,19 @@ int testOrder(size_t numThreads, size_t queueLog) {
 
 int main(int argc, const char **argv) {
   size_t numThreads;
-  for (numThreads = 1; numThreads <= 8; ++numThreads) {
-    size_t queueLog;
-    for (queueLog = 1; queueLog <= 8; ++queueLog) {
-      if (testOrder(numThreads, queueLog)) {
+  for (numThreads = 1; numThreads <= 4; ++numThreads) {
+    size_t queueSize;
+    for (queueSize = 1; queueSize <= 2; ++queueSize) {
+      if (testOrder(numThreads, queueSize)) {
         printf("FAIL: testOrder\n");
         return 1;
       }
     }
   }
   printf("PASS: testOrder\n");
-  (POOL_create(0, 1) || POOL_create(1, 0)) ? printf("FAIL: testInvalid\n")
-                                           : printf("PASS: testInvalid\n");
   (void)argc;
   (void)argv;
-  return 0;
+  return (POOL_create(0, 1) || POOL_create(1, 0))
+      ? printf("FAIL: testInvalid\n"), 1
+      : printf("PASS: testInvalid\n"), 0;
 }
