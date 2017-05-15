a31431870ca43cc2962fe97a6c5593436f8cf298
Nick Terrell <terrelln@fb.com>
2017-05-15 15:00:52 -0700

    [linux-kernel] Fix up a whitespace edge case

--- contrib/linux-kernel/spaces_to_tabs.sh
+++ contrib/linux-kernel/spaces_to_tabs.sh
@@ -2,27 +2,49 @@
 set -e
 
 # Constants
-INCLUDE='include/'
-LIB='lib/'
+INCLUDE='include/linux/'
+LIB='lib/zstd/'
 SPACES='    '
 TAB=$'\t'
 TMP="replacements.tmp"
 
-echo "Files: " $INCLUDE* $LIB*
-
-# Check files for existing tabs
-grep "$TAB" $INCLUDE* $LIB* && exit 1 || true
-# Replace the first tab on every line
-sed -i '' "s/^$SPACES/$TAB/" $INCLUDE* $LIB*
-
-# Execute once and then execute as long as replacements are happening
-more_work="yes"
-while [ ! -z "$more_work" ]
-do
-  rm -f $TMP
-  # Replaces $SPACES that directly follow a $TAB with a $TAB.
-  # $TMP will be non-empty if any replacements took place.
-  sed -i '' "s/$TAB$SPACES/$TAB$TAB/w $TMP" $INCLUDE* $LIB*
-  more_work=$(cat "$TMP")
+function prompt() {
+  while true; do
+    read -p "$1 [Y/n]" yn
+    case $yn in
+        '' ) yes='yes'; break;;
+        [Yy]* ) yes='yes'; break;;
+        [Nn]* ) yes=''; break;;
+        * ) echo "Please answer yes or no.";;
+    esac
 done
-rm -f $TMP
+}
+
+echo "Files: " $INCLUDE*.h $LIB*.{h,c}
+
+prompt "Do you wish to replace 4 spaces with a tab?"
+if [ ! -z "$yes" ]
+then
+  # Check files for existing tabs
+  grep "$TAB" $INCLUDE*.h $LIB*.{h,c} && exit 1 || true
+  # Replace the first tab on every line
+  sed -i '' "s/^$SPACES/$TAB/" $INCLUDE*.h $LIB*.{h,c}
+
+  # Execute once and then execute as long as replacements are happening
+  more_work="yes"
+  while [ ! -z "$more_work" ]
+  do
+    rm -f $TMP
+    # Replaces $SPACES that directly follow a $TAB with a $TAB.
+    # $TMP will be non-empty if any replacements took place.
+    sed -i '' "s/$TAB$SPACES/$TAB$TAB/w $TMP" $INCLUDE*.h $LIB*.{h,c}
+    more_work=$(cat "$TMP")
+  done
+  rm -f $TMP
+fi
+
+prompt "Do you wish to replace '{   ' with a tab?"
+if [ ! -z "$yes" ]
+then
+  sed -i '' "s/$TAB{   /$TAB{$TAB/g" $INCLUDE*.h $LIB*.{h,c}
+fi
